5a3fe2fc468617c7262c8a98d4c87264821ac695
hxj-2888 <hxj-2888@users.noreply.github.com>
2026-08-29 22:20:42 +0800

    chore: 清理调参临时脚本

--- 公测3.1/peek5.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import io
-import sys
-
-sys.stdout.reconfigure(encoding='utf-8')
-s = io.open('太空杀_公测3.1_模拟代码.py', encoding='utf-8').read()
-i = s.find('def ai_pick_kill_target')
-print(s[i:i + 1400])
--- 公测3.1/peek6.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import io
-import sys
-
-sys.stdout.reconfigure(encoding='utf-8')
-s = io.open('太空杀_公测3.1_模拟代码.py', encoding='utf-8').read()
-i = s.find('def apply_infection')
-print(s[i:i + 1000])
